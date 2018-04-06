8c4af5252ba037c280ea2d9304400a417488bfcb
Dominik Stamm <dominik.stamm@me.com>
2018-04-06 16:02:56 +0200

    removed build folder

--- src/lib/build/GF2nArithmeticUtils.d
+++
@@ -1,4 +0,0 @@
-build/GF2nArithmeticUtils.o: src/GF2nArithmeticUtils.cc \
-  src/../include/GF2nArithmetic.h include/CumffaTypes.h \
-  include/GF2nArithmeticUtils.h /Users/dom/opt/include/gmpxx.h \
-  /Users/dom/opt/include/gmp.h
--- src/lib/build/test.d
+++
@@ -1,2 +0,0 @@
-build/test.o: src/test.cc src/../include/GF2nArithmetic.h include/CumffaTypes.h \
-  include/GF2nArithmeticUtils.h
